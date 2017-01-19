991e9bfb252ffe1588ced22bfcfb4e68a393acaf
bytabo <jo@bytabo.de>
2017-01-19 15:37:08 +0100

    bootstrap grid

--- wordpress/wp-content/themes/theme/functions.php
+++ wordpress/wp-content/themes/theme/functions.php
@@ -3,26 +3,61 @@
 
 add_filter('show_admin_bar', '__return_false');
 
-function get_the_slug($id='') {
-    if(empty($id)) {
+function get_the_slug($id = '')
+{
+    if (empty($id)) {
         $id = get_the_ID();
     }
     $page = get_post($id);
-    if(is_object($page)) {
+    if (is_object($page)) {
         return $page->post_name;
     }
 }
 
-function the_slug($id='') {
+function the_slug($id = '')
+{
     echo get_the_slug($id);
 }
 
-function get_page_by_slug($slug='') {
+function get_page_by_slug($slug = '')
+{
     $page = get_posts(
         array(
-            'name'      => $slug,
+            'name' => $slug,
             'post_type' => 'page'
         )
     );
     return array_pop($page);
+}
+
+//e.g. call can be put_into_responsive_grid(array("col1", "col2"), 4, 'sm'); so a row with <div class="col-sm-3">col1</div> etc. is created
+function put_into_responsive_grid($array, $cols = 3, $breakpoint = 'md')
+{
+    $return_string = '';
+    if (!empty($array)) {
+        $add_row = true;
+        $first = true;
+        $counter = 0;
+        $cols_width = 12 / $cols;
+        foreach ($array as $a) :
+            if ($add_row) {
+                if ($first) {
+                    $first = false;
+                } else {
+                    $return_string .= "</div>";
+                }
+                $return_string .= "<div class=\"row\">";
+            }
+            $return_string .= "<div class=\"col-" . $breakpoint . "-" . $cols_width . "\">";
+            $return_string .= $a;
+            $return_string .= "</div>";
+
+            if (++$counter % $cols == 0)
+                $add_row = true;
+            else
+                $add_row = false;
+        endforeach;
+        $return_string .= "</div>";
+    }
+    return $return_string;
 }
